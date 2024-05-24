b973b2403a7d67927655513dec29070eb2728613
Segolen Plihon <131832637+splihon@users.noreply.github.com>
2024-05-23 23:02:44 -0700

    chococat webpage WIP

---
+++ B_Characters/Character4/js/lab.js
@@ -0,0 +1,8 @@
+// Wait until the document is fully loaded
+$(document).ready(function() {
+    // When the menu button is clicked
+    $('.menu-button').click(function() {
+        // Toggle the visibility of the menu
+        $('.menu').toggle();
+    });
+});
