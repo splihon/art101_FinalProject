0f9a163020981cf6f867660b6cb5a95a2b3e11de
bldmochi <natalie.p583@gmail.com>
2024-05-27 16:40:32 -0700

    working on my melody

---
+++ B_Characters/Character1/js/lab.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    // When the menu button is clicked
+    $('.menu-button').click(function() {
+        // Toggle the visibility of the menu
+        $('.menu').toggle();
+    });
+});
