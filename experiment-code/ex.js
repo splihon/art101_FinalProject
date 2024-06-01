e1dd723dfa687474ed7d981ed0c8cfc592c96c6a
3llaj3llaa <166079120+3llaj3llaa@users.noreply.github.com>
2024-06-01 00:03:27 -0700

    experimental button
    created a button which will generate a random sanrio character and some text

---
+++ experiment-code/ex.js
@@ -0,0 +1,18 @@
+//A button that generates a photo of a random sanrio character
+document.getElementById('generateButton').addEventListener('click', generateRandomContent);
+
+
+const imageTextPairs = [
+    {src: 'img/chocoCat.png', text: 'This is the first text'},
+    {src: 'img/kuromi.png', text: 'This is the second text'},
+    {src: 'img/myMelody.jpg', text: 'This is the third text'},
+    //add more img and correct text later
+];
+
+function generateRandomContent() {
+    const randomIndex = Math.floor(Math.random() * imageTextPairs.length);
+    const randomPair = imageTextPairs[randomIndex];
+
+    document.getElementById('randomImage').src = randomPair.src;
+    document.getElementById('randomText').innerText = randomPair.text;
+}
