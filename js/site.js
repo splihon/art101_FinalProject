82316f0ed360e471a7d9ea5a821c687b15c4d648
halberd69 <hfung2015@gmail.com>
2024-06-04 17:30:52 -0700

    losing my mind

--- js/site.js
+++ js/site.js
@@ -4,14 +4,9 @@ document.getElementById('generateButton').addEventListener('click', generateRand
 
 
 const imageTextPairs = [
-    {src: 'img-stickers/chococat.webp', text: 'This is the first text'},
-    {src: 'img-stickers/Baku_2D.webp', text: 'This is the second text'},
-    {src: 'img-stickers/transparentPurin.png', text: 'This is the third text'},
-    {src: 'img-stickers/transparent-kuromi.gif', text: 'This is the fourth text'},
-    {src: 'img-stickers/Mocha.webp', text: 'This is the fifth text'},
-    {src: 'img-stickers/my-melody.png', text: 'This is the sixth text'},
-    {src: 'img-stickers/pino.png', text: 'This is the seventh text'},
-    {src: 'img-stickers/usahana.webp', text: 'This is the eighth text'},
+    {src: 'img/chocoCat.png', text: 'This is the first text'},
+    {src: 'img/kuromi.png', text: 'This is the second text'},
+    {src: '../img/transparentPurin.png', text: 'This is the third text'},
     //add more img and correct text later
 ];
 
