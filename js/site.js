a09f670b7e68f8ac3be8496475c0771abe4abea8
halberd69 <hfung2015@gmail.com>
2024-06-05 23:07:39 -0700

    moved the code to the index html

--- js/site.js
+++ js/site.js
@@ -1,24 +1,16 @@
-
 //A button that generates a photo of a random sanrio character
 document.getElementById('generateButton').addEventListener('click', generateRandomContent);
 
 
-// const imageTextPairs = [
-//     {src: 'img/chocoCat.png', text: 'This is the first text'},
-//     {src: 'img/kuromi.png', text: 'This is the second text'},
-//     {src: '../img/transparentPurin.png', text: 'This is the third text'},
-//     //add more img and correct text later
-// ];
-
 const imageTextPairs = [
-    {src: 'img-stickers/chococat.webp', text: 'This is the first text'},
-    {src: 'img-stickers/Baku_2D.webp', text: 'This is the second text'},
-    {src: 'img-stickers/transparentPurin.png', text: 'This is the third text'},
-    {src: 'img-stickers/transparent-kuromi.gif', text: 'This is the fourth text'},
-    {src: 'img-stickers/Mocha.webp', text: 'This is the fifth text'},
-    {src: 'img-stickers/my-melody.png', text: 'This is the sixth text'},
-    {src: 'img-stickers/pino.png', text: 'This is the seventh text'},
-    {src: 'img-stickers/usahana.webp', text: 'This is the eighth text'},
+    {src: 'img/img-character/chococat.gif', text: 'This is the first text'},
+    // {src: 'img-stickers/Baku_2D.webp', text: 'This is the second text'},
+    {src: 'img/img-character/pompompurin.gif', text: 'This is the third text'},
+    {src: 'img/img-character/kuromigif.gif', text: 'This is the fourth text'},
+    {src: 'img/img-character/mochaSparklingEyes.gif', text: 'This is the fifth text'},
+    {src: 'img/img-character/my-melody.gif', text: 'This is the sixth text'},
+    // {src: 'img-stickers/pino.png', text: 'This is the seventh text'},
+    {src: 'img/img-character/usahana resized.png', text: 'This is the eighth text'},
     //add more img and correct text later
 ];
 
@@ -26,6 +18,19 @@ function generateRandomContent() {
     const randomIndex = Math.floor(Math.random() * imageTextPairs.length);
     const randomPair = imageTextPairs[randomIndex];
 
+//API Stuff here, it is a advice generator
+// then, we linked it up so that the characters "give" the user advice when you press generate
+    $.ajax({
+        url: 'https://api.adviceslip.com/advice',
+        type: 'GET',
+        success: function(response) {
+          const advice = JSON.parse(response).slip.advice;
+          document.getElementById('randomText').innerText = advice;
+        }
+      });
+
     document.getElementById('randomImage').src = randomPair.src;
-    document.getElementById('randomText').innerText = randomPair.text;
-}
+    
+
+
+};
